b8fb0a05596dacb9f66d36338b524dc8676791d6
Eduardo Hernandez <edhernandez@birt.eus>
2023-04-30 22:40:16 +0200

    Proyecto Spring Boot creado

---
+++ src/main/java/eus/birt/dam/app/Ud7TareaEvaluativa1Ejercicio1Application.java
@@ -0,0 +1,13 @@
+package eus.birt.dam.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud7TareaEvaluativa1Ejercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud7TareaEvaluativa1Ejercicio1Application.class, args);
+	}
+
+}
